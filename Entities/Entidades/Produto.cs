bcfd26dbf13e37e6d1f91a0d004c0bf5d412175c
Pedro henrique <pedrohenriquesiilvago@gmail.com>
2024-11-23 17:14:50 -0300

    Criação interface produto

--- Entities/Entidades/Produto.cs
+++ Entities/Entidades/Produto.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Entidades.Notificacoes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entidades.Entidades;
-public class Produto
+namespace Entidades.Entidades
 {
+
+    [Table("Product")]
+    public class Produto : Notifica
+    {
+
+        [Column("PRD_ID")]
+        [Display(Name = "Código")]
+
+        public int Id { get; set; }
+
+        [Column("PRD_NOME")]
+        [Display(Name = "Nome")]
+
+        public string Nome { get; set; }
+
+        [Column("PRD_VALOR")]
+        [Display(Name = "Valor")]
+
+        public decimal Valor { get; set; }
+
+        [Column("PRD_ESTADO")]
+        [Display(Name = "Estado")]
+
+        public bool Estado { get; set; }
+
+    }
 }
