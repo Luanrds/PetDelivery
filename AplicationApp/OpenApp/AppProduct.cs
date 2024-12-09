f212ba531630812dfefb78cfbd601032775cc84b
Luanrds <luanrsilva209@gmail.com>
2024-12-09 20:05:38 -0300

    Refatoração

--- AplicationApp/OpenApp/AppProduct.cs
+++ AplicationApp/OpenApp/AppProduct.cs
@@ -3,47 +3,46 @@ using Dominio.Interfaces.InterfaceProducts;
 using Dominio.Interfaces.InterfaceServices;
 using Entidades.Entidades;
 
-namespace Aplicacao.OpenApp
+namespace Aplicacao.OpenApp;
+
+public class AppProduct(IProduct IProduct, IServiceProduct IServiceProduct) : InterfaceProductApp
 {
-    public class AppProduct(IProduct IProduct, IServiceProduct IServiceProduct) : InterfaceProductApp
-    {
-		private readonly IProduct _IProduct = IProduct;
+	private readonly IProduct _IProduct = IProduct;
 
-		private readonly IServiceProduct _IServiceProduct = IServiceProduct;
+	private readonly IServiceProduct _IServiceProduct = IServiceProduct;
 
-		public async Task AddProduct(Produto produto)
-        {
-            await _IServiceProduct.AddProduct(produto);
-        }
+	public async Task AddProduct(Produto produto)
+	{
+		await _IServiceProduct.AddProduct(produto);
+	}
 
-        public async Task UpdateProduct(Produto produto)
-        {
-            await _IServiceProduct.UpdateProduct(produto);
-        }
+	public async Task UpdateProduct(Produto produto)
+	{
+		await _IServiceProduct.UpdateProduct(produto);
+	}
 
-        public async Task Add(Produto Objeto)
-        {
-            await _IProduct.Add(Objeto);
-        }
+	public async Task Add(Produto Objeto)
+	{
+		await _IProduct.Add(Objeto);
+	}
 
-        public async Task Delete(Produto Objeto)
-        {
-            await _IProduct.Delete(Objeto);
-        }
+	public async Task Delete(Produto Objeto)
+	{
+		await _IProduct.Delete(Objeto);
+	}
 
-        public async Task<Produto> GetEntityById(int Id)
-        {
-           return await _IProduct.GetEntityById(Id);
-        }
+	public async Task<Produto> GetEntityById(int Id)
+	{
+		return await _IProduct.GetEntityById(Id);
+	}
 
-        public async Task<List<Produto>> List()
-        {
-            return await _IProduct.List();
-        }
+	public async Task<List<Produto>> List()
+	{
+		return await _IProduct.List();
+	}
 
-        public async Task Update(Produto Objeto)
-        {
-            await _IProduct.Update(Objeto);
-        }
-    }
+	public async Task Update(Produto Objeto)
+	{
+		await _IProduct.Update(Objeto);
+	}
 }
