2a22c1617805db11b89f5b9b21c1641b10eebf9e
Luanrds <luanrsilva209@gmail.com>
2024-12-15 16:23:47 -0300

    Ajuste na camada de Infra estrutura

---
+++ Infrastucture/Configuracao/PetDeliveyContext.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastucture.Configuracao;
+
+public class PetDeliveyContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PetDeliveyContext).Assembly);
+    }
+}
---
+++ Infrastucture/InjecaoDeDependenciaExtensao.cs
@@ -0,0 +1,31 @@
+﻿using Dominio.Repositorios.Produto;
+using Infrastucture.Configuracao;
+using Infrastucture.Repositorio.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastucture;
+
+public static class InjecaoDeDependenciaExtensao
+{
+    public static void AdicioneInfraestrutura(this IServiceCollection services)
+    {
+        AdicioneRepositorios(services);
+    }
+
+    private static void AddDbContext_Npga(IServiceCollection services)
+    {
+        var connectionString = "Host=localhost;Port=5432;Database=petDelivey;Username=postgres;Password=0001";
+
+        services.AddDbContext<PetDeliveyContext>(dbContext =>
+        {
+            dbContext.UseNpgsql(connectionString);
+        });
+    }
+
+    private static void AdicioneRepositorios(IServiceCollection services)
+    {
+        services.AddScoped<IProdutoWriteOnly, ProdutoRepository>();
+        services.AddScoped<IProdutoReadOnly, ProdutoRepository>();
+    }
+}
