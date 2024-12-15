97953f98a895397a17b567a16e6270a52faca87c
Luanrds <luanrsilva209@gmail.com>
2024-12-15 16:24:33 -0300

    Ajuste para criação de novas estruturas

---
+++ PetDelivery.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace PetDelivery.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : PetDeliveryExceptions
+{
+    public IList<string> MensagensDeErro { get; set; }
+
+    public ErrorOnValidationException(IList<string> mensagensDeErro)
+    {
+        MensagensDeErro = mensagensDeErro;
+    }
+}
