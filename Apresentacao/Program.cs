62458b6ad04f44b8945f125562006350a6ef6c4f
Luanrds <luanrsilva209@gmail.com>
2024-12-16 18:11:40 -0300

    Refatoração

--- Apresentacao/Program.cs
+++ Apresentacao/Program.cs
@@ -4,19 +4,12 @@ using PetDelivery.API.Filtros;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddScoped<ProdutoFacade>();
-
-//builder.Services.AddScoped<ProdutoRepository>();
-
 builder.Services.AddControllers();
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 builder.Services.AdicioneAplicacao();
 builder.Services.AdicioneInfraestrutura(builder.Configuration);
-
-//builder.Services.AddDbContext<PetDeliveyContext>(options =>
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
