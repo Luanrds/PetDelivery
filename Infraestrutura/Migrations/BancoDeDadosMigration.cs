b637ed63e011985befe2395605d73fd40b4abd03
Luanrds <luanrsilva209@gmail.com>
2025-02-09 21:39:32 -0300

    Ajuste na organização de pastas

---
+++ Infraestrutura/Migrations/BancoDeDadosMigration.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace Infraestrutura.Migrations;
+
+public static class BancoDeDadosMigration
+{
+    public static void Migrate(string connectionString, IServiceProvider serviceProvider)
+    {
+        GaranteCriacaoDoBancoDeDados(connectionString);
+        MigrationBancoDeDados(serviceProvider);
+    }
+
+    private static void GaranteCriacaoDoBancoDeDados(string connectionString)
+    {
+        NpgsqlConnectionStringBuilder connectionStringBuilder = new(connectionString);
+
+        string? NomeDoBancoDeDados = connectionStringBuilder.Database;
+
+        connectionStringBuilder.Database = "postgres";
+
+        using var dbConnection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+
+        IEnumerable<dynamic> records = dbConnection.Query(
+            "SELECT datname FROM pg_database WHERE datname = @nome;", 
+            new { nome = NomeDoBancoDeDados });
+
+        if (!records.Any())
+        {
+            var query = $"CREATE DATABASE \"{NomeDoBancoDeDados}\";";
+            dbConnection.Execute(query);
+        }
+    }
+
+    private static void MigrationBancoDeDados(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        runner.MigrateUp();
+    }
+}
---
+++ Infraestrutura/Migrations/VersoesDeBancoDeDados.cs
@@ -0,0 +1,7 @@
+﻿namespace Infraestrutura.Migrations;
+
+public abstract class VersoesDeBancoDeDados
+{
+    public const int TABLE_PRODUTO = 1;
+    public const int TABLE_CARRINHO = 2;
+}
