2b3244e71e0ba83314bdc25e486d8526504554e9
Luanrds <luanrsilva209@gmail.com>
2025-02-20 23:47:50 -0300

    Refatoração

--- PetDelivery.Communication/Request/RequestProdutoJson.cs
+++ PetDelivery.Communication/Request/RequestProdutoJson.cs
@@ -3,10 +3,7 @@
 public class RequestProdutoJson
 {
     public string Nome { get; set; } = string.Empty;
-
     public decimal Valor { get; set; } = decimal.Zero;
-
     public bool Disponivel { get; set; } = false;
-
     public string Descricao { get; set; } = string.Empty;
 }
