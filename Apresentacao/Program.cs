a37f79681fa9e2348ee1e2409df63edc348eb093
Luanrds <luanrsilva209@gmail.com>
2025-04-27 01:22:58 -0300

    Configurando o Swagger para Incluir Tokens nas Requisições

--- Apresentacao/Program.cs
+++ Apresentacao/Program.cs
@@ -2,6 +2,7 @@ using Aplicacao;
 using Infraestrutura;
 using Infraestrutura.Extensoes;
 using Infraestrutura.Migrations;
+using Microsoft.OpenApi.Models;
 using PetDelivery.API.Conversoes;
 using PetDelivery.API.Filtros;
 using System.Text.Json.Serialization;
@@ -22,7 +23,36 @@ builder.Services.AdicioneAplicacao();
 builder.Services.AdicioneInfraestrutura(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				},
+				Scheme = "oauth2",
+				Name = "Bearer",
+				In = ParameterLocation.Header
+			},
+			new List<string>()
+		}
+	});
+});
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 var corsPolicy = "_myAllowSpecificOrigins";
