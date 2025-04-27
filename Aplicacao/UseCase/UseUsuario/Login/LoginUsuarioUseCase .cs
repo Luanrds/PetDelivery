090a376cbc2a1eab28a101dc7e2d8ae69ff4ccf2
Luanrds <luanrsilva209@gmail.com>
2025-04-26 23:32:02 -0300

    Refatoração da classe LoginUsuarioUseCase e criação de Exceção para login inválido.
    `LoginUsuarioUseCase` foi atualizado para usar `ISenhaEncripter` para criptografia de senhas
    e a lógica de recuperação de usuário foi substituída por `GetByEmailESenha`.
    `LoginInvalidoException` foi introduzido para lidar com tentativas de login inválidas
    com uma mensagem de erro personalizada e status HTTP 401. Instruções ``using` foram adicionadas para novas dependências.

--- Aplicacao/UseCase/UseUsuario/Login/LoginUsuarioUseCase .cs
+++ Aplicacao/UseCase/UseUsuario/Login/LoginUsuarioUseCase .cs
@@ -1,29 +1,31 @@
 ﻿using AutoMapper;
 using Dominio.Entidades;
 using Dominio.Repositorios.Usuario;
+using Dominio.Seguranca.Criptografia;
 using PetDelivery.Communication.Request;
 using PetDelivery.Communication.Response;
+using PetDelivery.Exceptions.ExceptionsBase;
 
 namespace Aplicacao.UseCase.UseUsuario.Login;
 public class LoginUsuarioUseCase : ILoginUsuarioUseCase
 {
 	private readonly IUsuarioReadOnly _usuarioReadOnly;
+	private readonly ISenhaEncripter _senhaEncripter;
 	private readonly IMapper _mapper;
 
-	public LoginUsuarioUseCase(IUsuarioReadOnly usuarioReadOnly, IMapper mapper)
+	public LoginUsuarioUseCase(IUsuarioReadOnly usuarioReadOnly, ISenhaEncripter senhaEncripter, IMapper mapper)
 	{
 		_usuarioReadOnly = usuarioReadOnly;
+		_senhaEncripter = senhaEncripter;
 		_mapper = mapper;
 	}
 
 	public async Task<ResponseUsuarioJson?> ExecuteAsync(RequestLoginUsuarioJson request)
 	{
-		Usuario? usuario = await _usuarioReadOnly.GetByEmail(request.Email);
+		string senhaEncripitada = _senhaEncripter.Encrypt(request.Senha);
 
-		if (usuario is null || usuario.Senha != request.Senha)
-		{
-			return null;
-		}
+		Usuario? usuario = await _usuarioReadOnly.GetByEmailESenha(request.Email, senhaEncripitada)
+			?? throw new LoginInvalidoException();
 
 		var response = _mapper.Map<ResponseUsuarioJson>(usuario);
 		return response;
